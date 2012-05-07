5c6633a264b27a59d70f353e46260bbd935d0568
Uiri <uiri00@gmail.com>
2012-05-07 14:22:48 -0400

    fix create user function

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express')
   , fs = require('fs')
+  , FB = require('fb')
   , crypto = require('crypto')
   , everyauth = require('everyauth')
   , configfile = require('./config.js')
@@ -18,9 +19,17 @@ function insertCallback(err, body) {
 
 function createUser(fbdata) {
     console.log(fbdata);
-    players[fbdata.id] = fbdata;
+    var id = fbdata.user_id
+    players[id] = new Object;
+    players[id].id = fbdata.user_id;
+    FB.api(fbdata.user_id, { fields: ['name'] }, function (res) {
+	if (res && !res.error)
+	    players[id].name = res.name;
+	else
+	    console.log(res.error);
+    });
     db.insert(players, 'users', insertCallback);
-    return players[fbdata.id];
+    return players[id];
 }
 
 db.get('incompletegames', function(err, body) {
@@ -48,8 +57,10 @@ everyauth.facebookCanvas
     .appId('304117199665452')
     .appSecret(configfile.appsecret)
     .findOrCreateUser( function(session, accessToken, accessTokenExtra, fbUserMetadata) {
-	return players[fbUserMetadata.id] ||
-	    createUser(fbUserMetadata);
+	if (fbUserMetadata.user_id)
+	    return players[fbUserMetadata.user_id]
+	else
+	    return createUser(fbUserMetadata);
     })
     .redirectPath('/')
 
