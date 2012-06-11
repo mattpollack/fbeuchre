cae5418ad53ed8ac38ef1a41260dc4d7fcaffcc7
Matthew <matt.pck@gmail.com>
2012-06-11 15:16:28 -0400

    Added endpoints.api

--- public/fbmenu.js
+++ public/fbmenu.js
@@ -45,7 +45,7 @@ function menu() {
 					var xmlhttp = new XMLHttpRequest();
 					xmlhttp.onreadystatechange = function() {
 						if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-					    	document.getElementById("myDiv").innerHTML = xmlhttp.responseText;
+					    	jsonres = JSON.parse(xmlhttp.responseText); // json response
 					    }
 					}
 					xmlhttp.open("POST","/newgame",true);
